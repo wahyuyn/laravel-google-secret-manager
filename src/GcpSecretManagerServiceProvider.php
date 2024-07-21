f314e730520359e1ad1e8b9a11767209a76343a4
wahyu <wahyu@centria.co.id>
2024-07-22 00:44:44 +0700

    initial Commit

---
+++ src/GcpSecretManagerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Vendor\GcpSecretManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class GcpSecretManagerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Boot methods
+    }
+
+    public function register()
+    {
+        // Register bindings
+    }
+}
